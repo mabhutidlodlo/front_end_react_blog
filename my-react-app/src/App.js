0f8aeb55c6617658fb81475e9b5fb4f6a0dff2f4
Bawinile <bawinilem@bbd.co.za>
2022-03-26 22:38:28 +0200

    Created a boilerplate for the blog

---
+++ my-react-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          We are building a blog!
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
